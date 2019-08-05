5bb609e678c604d0fa27504db0d4dca416ad24b8
Cristhian David Narvaez Campiño <analista5@mercadeo-virtual.com>
2019-08-05 10:16:51 -0500

    pagina de inicio infoporcinos

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ import check from '../src/icons/check.svg';
 import user from '../src/icons/user.svg';
 import update from '../src/icons/update.svg';
 import WhiteMenu from './components/whitemenu.js';
+import central from '../src/images/CG-logo.svg';
+import pig2 from '../src/images/2.png';
+import pig3 from '../src/images/3.png';
+import { MDBCarouselInner, MDBContainer, MDBCarouselItem, MDBView, MDBCarousel  } from "mdbreact";
 
 
 class App extends React.Component {
@@ -24,36 +28,179 @@ class App extends React.Component {
 
       <div >
 
+
         <NavbarTest />
         <WhiteMenu />
 
-
         <Row>
-          <Col md={12}>
+          <Col md={12} lg={12} sm={12}>
+
             <img className="pigsinicio" src={pigs} />
-
-            <div className="t1">APROVECHA AL MÁXIMO TODOS TUS RECURSOS </div>
-            <div className="t2">El mejor sistema de <br />
-              información  </div>
-            <div className="t3">para tu negocio</div>
-
-
-
+            <img className="desktopp" src={desktop} />
 
           </Col>
 
+          </Row>
 
-        </Row>
-        <Row>
-          <Col md={4}>1 of 3</Col>
-          <Col md={4}>2 of 3</Col>
-          <Col md={4}>3 of 3</Col>
-        </Row>
+          <Row>
+
+          <Col xs="12" md="2">
+           </Col>
+
+            <Col xs="12" md="8">
+
+            <div className="t4">Trazabilidad en Tiempo Real</div>
+              <div className="t5">Acerca de Infoporcinos</div>
+              <div className="t6">
+
+                <p>
+
+                  Somos la mejor plataforma de Información para el sector de la carne de cerdo. Buscamos crear e <br />
+                  innovar en las nuevas tecnologías para integrar, intermediar e informar en Tiempo Real (TR) las condiciones <br />
+                  técnicas de calidad de la carne de porcinos.
+    
+                </p>
+
+              </div>
+
+              <div class="text-center"><button class="ButtonAcerca" type="button">VER MAS</button></div>
+
+              
+            </Col>
+
+            <Col xs="12" md="2">
+            </Col>
+
+          </Row>
+
+     
+          <div className="full-container text-center">
+            <div class="textos">
+              <h5>La Mejor Optimización </h5>
+              <h2>¡Nuestro Nuevo Módulo!</h2>
+            </div>
+            <div class="row">
+              <div class="col-lg-3 col-md-12 col-sm-12">
+
+                <div class="muestra">
+
+                  <div class="item">
+                    <div class="circulo2">
+                      <img class="iconos" src={pen}></img>
+                    </div>
+                    <div class="contenido">
+                      <p className="titulos" >Diseño estético e interactivo</p>
+                      <span class="text">Lorem Ipsum dolor sit met, consectetur adipiscing elit, sed do eiusmod tempor.</span>
+                    </div>
+                  </div>
+
+                  <div class="item">
+                    <div class="circulo2">
+                      <img class="iconos" src={check}></img>
+                    </div>
+                    <div class="contenido">
+
+                    <p className="titulos" >Mantente al día</p>
+                      <span class="text">Lorem Ipsum dolor sit met, consectetur adipiscing elit, sed do eiusmod tempor.</span>
+                    </div>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div class="col-lg-6 col-md-12 col-sm-12">
+                <div class="muestra">
+                  
+                  <img class="pantalla" src={desktop}></img>
+
+                </div>
+
+              </div>
+
+              <div class="col-lg-3 col-md-12 col-sm-12">
+
+                <div class="muestra">
+
+                  <div class="item">
+                    <div class="circulo2">
+                      <img class="iconos" src={user}></img>
+                    </div>
+                    <div class="contenido">
+
+                    <p className="titulos" >Experiencia simple y óptima</p>
+                      <span class="text">Lorem Ipsum dolor sit met, consectetur adipiscing elit, sed do eiusmod tempor.</span>
+                    </div>
+                  </div>
+
+                  <div class="item">
+                    <div class="circulo2">
+                      <img class="iconos" src={update}></img>
+                    </div>
+                    <div class="contenido">
+
+                      <p className="titulos" >Actualizaciones frecuentes</p>
+                      <span class="text">Lorem Ipsum dolor sit met, consectetur adipiscing elit, sed do eiusmod tempor.</span>
+                    </div>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+
+            </div>
+
+          </div>
+
+          <MDBContainer className="carousel_inicio">
+      <MDBCarousel
+        activeItem={1}
+        length={3}
+        showControls={true}
+        showIndicators={true}
+        className="z-depth-1"
+      >
+        <MDBCarouselInner>
+          <MDBCarouselItem itemId="1">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src={central}
+                alt="First slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="2">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src={pig2}
+                alt="Second slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="3">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src={pig3}
+                alt="Third slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+        </MDBCarouselInner>
+      </MDBCarousel>
+    </MDBContainer>
+  );
 
 
         <Footer />
-      </div>
 
+
+      </div>
 
     );
   }
